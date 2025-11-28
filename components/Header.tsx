0d3afebc4ac6ca3f0cd8a7f9f2f32076456da73b
Sumit Bajaj <email.bajaj@gmail.com>
2025-11-28 07:55:24 +0530

    Update blog link in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
                     <h1 className="white">Skip the theory.</h1>
                     <h1 className="white">Let's discuss the code!</h1>
                     <p className="gray">I love to design and code complex problems into beautiful simple things.</p>
-                    <a className="green" href="http://blogs.bajajsumit.com">Sneak peek into some of my craftwork</a>
+                    <a className="green" href="http://blogs.sumitbajaj.com">Sneak peek into some of my craftwork</a>
                     {/* <a className="green" href="mailto:email.bajaj@gmail.com">Let&#39;s connect!</a> */}
                 </div>
                 <Image src="/images/sumit-profile.png" width={713} height={513} alt="sumit-profile"  /> 
